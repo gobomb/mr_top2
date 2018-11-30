ace5c0b71caa720f2ea7fa970e088858aa44c2bd
yiming chen <bombcoder@outlook.com>
2018-11-30 09:51:45 +0800

    updata getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -4,6 +4,6 @@ export DATA_PATH=/root/input
 export OUTPUT_PATH=/root/output
 export HADOOP_PREFIX=/usr/local/hadoop
 
-curl -o ${DATA_PATH}/data  ${DATA_PRODUCE}/output.txt
+curl -o ${DATA_PATH}/data  ${DATA_PRODUCE}/data.txt
 ${HADOOP_PREFIX}/bin/hadoop fs -mkdir -p /kaola/order/
 ${HADOOP_PREFIX}/bin/hadoop fs -moveFromLocal ${DATA_PATH}/data /kaola/order/input
